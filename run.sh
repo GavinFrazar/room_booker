1664cd28c34108a58744aefadcbee7ba2a1c6b7c
Gavin <gavinfrazar@gmail.com>
2018-09-30 14:57:07 -0700

    bash script update

--- run.sh
+++ run.sh
@@ -1,6 +1,13 @@
-python room_booker.py 0 &
-python room_booker.py 1 &
-python room_booker.py 2 &
-python room_booker.py 3 &
-python room_booker.py 4 &
-python room_booker.py 5 &
+#!/bin/bash
+for i in {0..5}
+do
+    python room_booker.py $i &
+done
+
+#options:
+#--from <days from now to begin booking range>
+#--to <days from now to end booking range>
+#--reset makes recent bookings reset
+#--headless <true/false>
+#--earliest-time <time of day>
+#--room <room number to book>
